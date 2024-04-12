164f853e00de71689250483a45e88f8d0cd28adf
rekhasekar24 <121107567+rekhasekar24@users.noreply.github.com>
2024-04-12 23:53:10 +0530

    Add files via upload
    chatbot

---
+++ chatbot4/response.js
@@ -0,0 +1,8 @@
+const responseObj = {
+    hello: "Hey ! How are you doing ?",
+    hey: "Hey! What's Up",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    programminglanguage: "A programming language is a system of notation for writing computer programs. The source code for a computer program in C.",
+    myname :"rekha",  
+  };
